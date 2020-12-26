57d3436a71e52ca5a3d4c52eec7115e56a222887
yarin1994 <yarin1994@gmail.com>
2020-12-26 18:35:17 +0200

    game.router

--- Routers/game.router.js
+++ Routers/game.router.js
@@ -1,1 +1,12 @@
-//hello
+const { Router } = require('express');
+const { gameController } = require('../Controllers/game.ctrl');
+
+const gmaeRouter = new Router();
+module.exports = { gameRouter };
+
+gameRouter.get('/', gameController.getGames);
+gameRouter.get('/:id', gameController.getGame);
+gameRouter.post('/', gameController.addGame);
+gameRouter.put('/:id', gameController.updateGame);
+gameRouter.delete('/:id', gameController.deleteGame);
+
