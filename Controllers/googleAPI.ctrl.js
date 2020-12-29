3d497fca6bb8532f2de6882e86411b79b85b3a99
yalisofer94 <yalisofer@gmail.com>
2020-12-29 21:45:52 +0200

    Google places API server & client done

--- Controllers/googleAPI.ctrl.js
+++ Controllers/googleAPI.ctrl.js
@@ -4,6 +4,12 @@ const axios = require('axios').default;
 exports.googleAPIController = {
 
     getRestFromAPI(req, res) {
-
+        if(req.query.length != 0) {
+            console.log("Hey Mate");
+            axios
+                .get(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${req.query.restName}&inputtype=textquery&fields=geometry&key=AIzaSyBkxP0uOzCNjtByiZD1KccRs7GFfKy_7ss`)
+                .then((response) => {res.json(response.data.candidates[0].geometry.location); })
+                .catch(err => console.log(`Error is: ${err}`));
+        }
     }
 }
--- Controllers/restaurant.ctrl.js
+++ Controllers/restaurant.ctrl.js
@@ -3,43 +3,17 @@ const axios = require('axios').default;
 exports.restaurantController = {
 
     getRestaurants(req, res) {
-        console.log(req.query.restName);
-        if(req.query.length != 0) {
-            console.log("Hey Mate");
-            axios
-                .get(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${req.query.restName}&inputtype=textquery&fields=geometry&key=AIzaSyBkxP0uOzCNjtByiZD1KccRs7GFfKy_7ss`)
-                .then(res => {console.log(res.data.candidates[0].geometry.location)})
-                .catch(err => console.log(`Error is: ${err}`));
-
-        }
-        // Restaurant.find({})
-        //     .then(docs => {
-        //         res.json(docs)
-        //     })
-        //     .catch(err => console.log(`Error, could NOT get to database: ${err}`));
+        Restaurant.find({})
+            .then(docs => {
+                res.json(docs)
+            })
+            .catch(err => console.log(`Error, could NOT get to database: ${err}`));
     },
 
     getRestaurant(req, res) {
         Restaurant.findOne({ id: req.params.id })
             .then(docs => { res.json(docs) })
             .catch(err => console.log(`Error, could NOT get to database: ${err}`));
-        
-       
-
-
-
-        // function getRestaurants(str) {
-        //     $.ajax({
-        //         url: `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${str}&inputtype=textquery&fields=formatted_address,name,rating,geometry&key=AIzaSyBkxP0uOzCNjtByiZD1KccRs7GFfKy_7ss`,
-        //         type: 'GET',
-        //         success: function (restaurants) {
-        //             console.log(restaurants);
-        //             return restaurants;
-        //         }
-        //     });
-        // }
-
-
     },
 
     async addRestaurant(req, res) {
