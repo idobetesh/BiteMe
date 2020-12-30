f142b4f8def1db89c7f11937cc2bf5f5e5366539
yarin1994 <yarin1994@gmail.com>
2020-12-30 11:17:08 +0200

    push rest to mongo

--- Controllers/googleAPI.ctrl.js
+++ Controllers/googleAPI.ctrl.js
@@ -1,3 +1,5 @@
+const Restaurant = require('../models/restaurant');
+const restCtrl = require('../Controllers/restaurant.ctrl.js');
 const axios = require('axios').default;
 
 exports.googleAPIController = {
@@ -7,7 +9,9 @@ exports.googleAPIController = {
             console.log("Hey Mate");
             axios
                 .get(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${req.query.restName}&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyBkxP0uOzCNjtByiZD1KccRs7GFfKy_7ss`)
-                .then((response) => {res.json(response.data); })
+                .then((response) => {
+                    // console.log(response.data.candidates[0].name)
+                    res.json(response.data); })
                 .catch(err => console.log(`Error is: ${err}`));
         }
     }
