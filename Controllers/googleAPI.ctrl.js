61d8a5fb4e1db23ded2bdee2423a9b266cfc15fb
yarin1994 <yarin1994@gmail.com>
2020-12-30 13:58:30 +0200

    nodemailer installed

--- Controllers/googleAPI.ctrl.js
+++ Controllers/googleAPI.ctrl.js
@@ -7,7 +7,7 @@ exports.googleAPIController = {
         if(req.query.length != 0) {
             console.log("Hey Mate");
             axios
-                .get(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${req.query.restName}&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyBkxP0uOzCNjtByiZD1KccRs7GFfKy_7ss`)
+                .get(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${req.query.restName}&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&Atmosphere=price_level&key=AIzaSyBkxP0uOzCNjtByiZD1KccRs7GFfKy_7ss`)
                 .then((response) => {res.json(response.data);})
                 .catch(err => console.log(`Error is: ${err}`));
         }
